9c1fa4e727d3ff6afa3710e6ab80ca5149be4fd5
owenramire <owenomar29@gmail.com>
2021-09-14 17:00:02 -0500

    Subscribe component to the context provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Routes from './routes/Routes';
+import PokemonProvider from './context/pokemons/PokemonProvider';
+
 import './styles/index.css';
 
 const App = () => {
   return (
-    <Routes />
+    <PokemonProvider>
+      <Routes />
+    </PokemonProvider>
   );
 }
 
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,4 +1,16 @@
+import { useContext, useEffect } from 'react';
+import PokemonContext from '../../context/pokemons/PokemonContext';
+
 const Home = () => {
+  // Assign the context value to use in this component
+  const myPokemonContext = useContext(PokemonContext);
+
+  useEffect(() => {
+    myPokemonContext.getPokemons().catch(null);
+  }, []);
+
+  console.log(myPokemonContext.pokemons);
+
   return (
     <div>
       <h1>Home</h1>
