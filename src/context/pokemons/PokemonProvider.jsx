910029299c1033cbf65971677d2de414c9e03443
owenramire <owenomar29@gmail.com>
2021-09-18 12:27:46 -0500

    Get pokemon details
    Create a method that will call at the API but with a different path,
    this time in the path we are going to pass tha pokemon id to have all
    its detail.

--- src/context/pokemons/PokemonProvider.jsx
+++ src/context/pokemons/PokemonProvider.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 const PokemonProvider = ({children}) => {
   const [pokemons, setPokemons] = useState([]);
+  const [pokemonDetail, setPokemonDetail] = useState({});
 
   const getPokemons = async () => {
     try {
@@ -14,13 +15,29 @@ const PokemonProvider = ({children}) => {
     }
   };
 
+  const getPokemonDetail = async (id) => {
+    if(!id) {
+      Promise.reject('The id is required');
+    }
+    try {
+      const pokemonResultDetail = await apiCall({url: `https://pokeapi.co/api/v2/pokemon/${id}`})
+      setPokemonDetail(pokemonResultDetail);
+    } catch(error) {
+      setPokemonDetail({});
+    }
+  }
+
   return (
     // Use a Provider to pass the current theme to the tree below.
     // Any component can read it, no matter how deep it is.
     
     // At this time the current value is an object with a property
     // and a method
-    <PokemonContext.Provider value={{ getPokemons, pokemons }}>
+    <PokemonContext.Provider value={{
+      getPokemons,
+      pokemons,
+      getPokemonDetail,
+      pokemonDetail }}>
       {children}
     </PokemonContext.Provider>
   );
